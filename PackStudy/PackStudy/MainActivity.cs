8ecdf62cac3326b679d819da6062d7794f0d0aa7
adsmit33 <austinsmith@nevada.unr.edu>
2017-03-22 15:45:35 -0700

    Returns JSON from PHP file and Add's it to User class

--- PackStudy/PackStudy/MainActivity.cs
+++ PackStudy/PackStudy/MainActivity.cs
@@ -6,19 +6,30 @@ using System.Net;
 using System.Collections.Specialized;
 using System.Text;
 using Android.Content;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace PackStudy
 {
     [Activity(Label = "PackStudy", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        
-        
+        string email;
+        string password;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
-            
+
+            //check the current sharedPrefrences to see if a user is signed in
+            ISharedPreferences sharedPrefrences = GetSharedPreferences("MyData", FileCreationMode.Private);
+            string CurrentUserFirstName = sharedPrefrences.GetString("FirstName", null);
+            string CurrentUserLastName = sharedPrefrences.GetString("LastName", null);
+            string message = "Welcome " + CurrentUserFirstName + " " + CurrentUserLastName;
+            Context context = ApplicationContext;
+            Toast toast = Toast.MakeText(context, message, ToastLength.Long);
+            toast.Show();
 
             Button btnLogin = (Button)FindViewById(Resource.Id.btnLogin);
             btnLogin.Click += BtnLogin_Click;
@@ -36,45 +47,59 @@ namespace PackStudy
             EditText txtEmail = (EditText)FindViewById(Resource.Id.txtEmail);
             EditText txtPassword = (EditText)FindViewById(Resource.Id.txtPassword);
 
-            string email = txtEmail.Text;
-            string password = txtPassword.Text;
+            email = txtEmail.Text;
+            password = txtPassword.Text;
 
             //set POST data for login.php
             parameter.Add("Email", email);
             parameter.Add("Password", password);
+            byte[] returnValue = client.UploadValues(uri, parameter);
+            string r = Encoding.ASCII.GetString(returnValue);
 
-            client.UploadValuesCompleted += Client_UploadValuesCompleted;
-            client.UploadValuesAsync(uri, parameter);
-
-        }
-
-        private void Client_UploadValuesCompleted(object sender, UploadValuesCompletedEventArgs e)
-        {
-            string data = Encoding.UTF8.GetString(e.Result);
-            //when echoing nothing it doesnt send NULL so you need to just see if there is an empty string
-            //if there is an empty string this means that no user was found so invalid username of password
-            if(data == "")
+            if (r == "")
             {
                 string message = "Invalid Email";
                 Context context = ApplicationContext;
                 Toast toast = Toast.MakeText(context, message, ToastLength.Long);
                 toast.Show();
             }
-            else if(data == "Invalid Password")
+            else if (r == "Invalid Password")
             {
                 Context context = ApplicationContext;
-                Toast toast = Toast.MakeText(context, data, ToastLength.Long);
+                Toast toast = Toast.MakeText(context, r, ToastLength.Long);
                 toast.Show();
             }
             else
             {
-                string message = "Welcome " + data;
+                List<User> users = JsonConvert.DeserializeObject<List<User>>(r);
+                User CurrentUser = users[0];
+                //since the user name and password work, store it in the shared prefrences 
+                ISharedPreferences sharedPrefrences = GetSharedPreferences("MyData", FileCreationMode.Private);
+                var prefEditor = sharedPrefrences.Edit();
+                prefEditor.PutInt("id", CurrentUser.Id);
+                prefEditor.PutString("FirstName", CurrentUser.FirstName);
+                prefEditor.PutString("LastName", CurrentUser.LastName);
+                prefEditor.PutString("Email", CurrentUser.Email);
+                prefEditor.PutString("Password", CurrentUser.Password);
+                prefEditor.PutString("PhoneNumber", CurrentUser.PhoneNumber);
+                prefEditor.PutString("Username", CurrentUser.Username);
+
+                prefEditor.Commit();
+
+                //get the current shared prefrences and display welcome message
+                string CurrentUserFirstName = sharedPrefrences.GetString("FirstName", null);
+                string CurrentUserLastName = sharedPrefrences.GetString("LastName", null);
+                string message = "Welcome " +CurrentUserFirstName+ " " + CurrentUserLastName;
                 Context context = ApplicationContext;
                 Toast toast = Toast.MakeText(context, message, ToastLength.Long);
                 toast.Show();
+
+
             }
-            
+
+
         }
+            
     }
 }
 
--- PackStudy/PackStudy/obj/Debug/android/src/mono/MonoPackageManager.java
+++ PackStudy/PackStudy/obj/Debug/android/src/mono/MonoPackageManager.java
@@ -99,6 +99,7 @@ class MonoPackageManager_Resources {
 	public static final String[] Assemblies = new String[]{
 		/* We need to ensure that "PackStudy.dll" comes first in this list. */
 		"PackStudy.dll",
+		"Newtonsoft.Json.dll",
 	};
 	public static final String[] Dependencies = new String[]{
 	};
